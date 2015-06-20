9d629040ae2bd62f51fa9e503243062d31c753fc
Florent H. CARRÉ <colundrum@users.noreply.github.com>
2015-06-20 12:16:11 +0200

    Update user_part.sh
    correct error

--- user_part.sh
+++ user_part.sh
@@ -2,7 +2,7 @@
 
 ### We set up the user used to access to the vogsphere
 
-mkdir -p ~/.ssh/config &&
+mkdir -p ~/.ssh &&
 cat >> ~/.ssh/config << EOF
 Host vgs vgs.42.fr vgse vgse.42.fr vogsphere vogsphere.42.fr vogsphere-exam vogsphere-exam.42.fr
 	User vogsphere
